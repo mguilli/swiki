99e7321c2ce8c154dd009f1838e65cf641b1197e
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-09-03 21:09:18 +0000

    updated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  has_many :collabs       
   has_many :wikis, through: :collabs
 
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,9 @@
 class Wiki < ActiveRecord::Base
+  has_many :collabs
   has_many :users, through: :collabs
+
+
+  def owner
+    collabs.where(owner: true).first.user
+  end
 end
