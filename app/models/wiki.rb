d34deac02d19f0b9ae565a5072820ca7cbe5d3a9
Matthew Guilliams <matthew.guilliams@gmail.com>
2014-09-05 04:52:08 +0000

    Added seed data for testing

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -6,4 +6,20 @@ class Wiki < ActiveRecord::Base
   def owner
     collabs.where(owner: true).first.user
   end
+
+  def self.create_wiki(user, title, public = true)
+    newwiki = Wiki.new(
+      title: title,
+      body: "This is a #{title} article.",
+      public: public
+    )
+    newwiki.save
+
+    newcollab = Collab.new(
+      user_id: user.id,
+      wiki_id: newwiki.id,
+      owner: true
+    )
+    newcollab.save  
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create free account user
+freeuser = User.new(
+  name: 'Free User',
+  email: 'freeuser@example.org',
+  password: 'password'
+)
+freeuser.skip_confirmation!
+freeuser.save
+
+# Create paid account user
+paiduser = User.new(
+  name: 'Paid User',
+  email: 'paiduser@example.org',
+  password: 'password',
+  premium_user: true
+)
+paiduser.skip_confirmation!
+paiduser.save
+
+# Create wiki articles and associated collab entries
+Wiki.create_wiki(freeuser, "Dog Poop")
+Wiki.create_wiki(freeuser, "Cat")
+Wiki.create_wiki(paiduser, "Rat")
+Wiki.create_wiki(paiduser, "Monkey", false)
+
+#Create collaboration on Monkey article
+Collab.create(
+  owner: false,
+  user_id: freeuser.id,
+  wiki_id: Wiki.last.id
+)
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Wiki.count} wikis created."
+puts "#{Collab.count} collaborations created."
+
+public 
+
